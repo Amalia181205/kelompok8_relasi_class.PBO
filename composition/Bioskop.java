5e7293a34d0d63735dcda432965056fd6f9bc9a7
Amalia181205 <lutvianiamalia@gmail.com>
2025-10-13 18:52:02 +0700

    update

--- composition/Bioskop.java
+++ composition/Bioskop.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 public class Bioskop {
     
      private String namaBioskop;
-     private ArrayList<Studio> daftarStudio = new ArrayList<>(); // composition
-
+     // Daftar studio yang dimiliki bioskop (studio tidak bisa berdiri tanpa bioskop)
+     private ArrayList<Studio> daftarStudio = new ArrayList<>(); 
+     
+     // Metode untuk menambahkan studio ke dalam daftar bioskop
     public Bioskop(String namaBioskop){
         this.namaBioskop = namaBioskop;
     }
 
+    // Metode untuk menambahkan studio ke dalam daftar bioskop
     public void tambahStudio(Studio studio){
         daftarStudio.add(studio);
     }
 
+    // Metode untuk menampilkan informasi bioskop dan seluruh studionya
     public void tampilkanBioskop(){
         System.out.println("Bioskop: " + namaBioskop);
         for(Studio s : daftarStudio){
--- composition/Studio.java
+++ composition/Studio.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 public class Studio {
     
     private String namaStudio;
+    // Daftar film yang ditayangkan di studio(film tidak ada tanpa studio)
     private ArrayList<Film> daftarFilm = new ArrayList<>(); // composition
 
     public Studio(String namaStudio){
         this.namaStudio = namaStudio;
     }
 
+    // Metode untuk menambahkan film baru ke dalam studio
     public void tambahFilm(String judul, int durasi){
         daftarFilm.add(new Film(judul, durasi));
     }
 
+    // Metode untuk menampilkan daftar film di studio
     public void tampilkanFilm(){
         System.out.println("Daftar film di " + namaStudio + " : ");
         for(Film f : daftarFilm){
